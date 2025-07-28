59e1d56bc2869c2de9346d12440c2f398d4c0571
aadarsh0507 <infor0507@gmail.com>
2025-07-28 10:42:06 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,7 @@ const api = axios.create({
   'https://push-web.onrender.com/api',
   // 'http://localhost:5000/api', // Change this to your backend URL/port
   // "https://5000-firebase-pushdiggygit-1751345989139.cluster-isls3qj2gbd5qs4jkjqvhahfv6.cloudworkstations.dev/api", // Change this to your backend URL/port
-  withCredentials: true, // Optional: if you use cookies/auth
+ 
 });
 
 export default api;
